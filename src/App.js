2e454942100a80560db33f91127ca2ef52e170fa
Challenge Handler <challengehandler@gmail.com>
2022-11-24 14:22:47 +0300

    feat: remove unneccesary files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import Bridge from "./pages/Bridge";
 import TopBar from "./components/TopBar";
 import Body from "./components/Body";
-import { Box, useMediaQuery } from "@mui/material";
-import styled from "styled-components";
 
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="w-screen flex flex-col">
-        <TopBar />
-        <Body />
-      </div>
-    </BrowserRouter>
+    <div className="w-screen flex flex-col">
+      <TopBar />
+      <Body />
+    </div>
   );
 }
 
